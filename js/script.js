2c6e5194d22695e2775a0c053a9a525c7ff2b611
Forklein <giuseppepisani2@gmail.com>
2021-07-05 15:33:20 +0200

    Final push

--- js/script.js
+++ js/script.js
@@ -12,19 +12,22 @@ var multiple5;
 var doublemultiple;
 
 for(var i = 1; i < 101; i++){
-    if(i % 3 === 0){
+    if(i % 3 === 0 && i % 5 === 0){
+        doublemultiple = 'FizzBuzz';
+        console.log(doublemultiple);
+    }else if(i % 3 === 0){
         multiple3 = 'Fizz';
         console.log(multiple3);
     }else if(i % 5 === 0){
         multiple5 = 'Buzz';
-        console.log(multiple5)
-    }else if(i % 3 === 0 && i % 5 === 0){
-        doublemultiple = 'FizzBuzz';
-        console.log(doublemultiple);
-    }else{
+        console.log(multiple5);
+    }
+    else{
         console.log(i);
     }
 }
+
+
 
 
 

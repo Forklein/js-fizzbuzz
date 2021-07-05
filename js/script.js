115d7c4d25abd7290d5b8c150b9c317fc42108e2
Forklein <giuseppepisani2@gmail.com>
2021-07-05 15:07:26 +0200

    Set for cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('Ciao Peppe')
+
+
+//! TRACK
+
+/*Scrivi un programma che stampi i numeri da 1 a 100.
+Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+
+for(var i = 1; i < 101; i++){
+    console.log(i)
+    if(i % 3 === 0){
+        REGOLA
+    }else if(i % 5 === 0){
+        REGOLA
+    }else if((i % 3 === 0) && (i % 5 === 0)){
+        REGOLA
+    }
+}
+

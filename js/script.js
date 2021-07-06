a2abcacfa6cdcb1e87dd8b130026f0689eff7ffe
Forklein <giuseppepisani2@gmail.com>
2021-07-06 13:06:09 +0200

    Add variable listItem for innerhtml

--- js/script.js
+++ js/script.js
@@ -8,24 +8,27 @@ Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 var result = document.getElementById('result');
+var listItem = '';
 
 
-for(var i = 1; i < 101; i++){
-    if(i % 3 === 0 && i % 5 === 0){
+for (var i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz');
-        result.innerHTML += '<li> FizzBuzz </li>';
-    }else if(i % 3 === 0){
+        listItem += '<li> FizzBuzz </li>';
+    } else if (i % 3 === 0) {
         console.log('Fizz');
-        result.innerHTML += '<li> Fizz </li>';
-    }else if(i % 5 === 0){
+        listItem += '<li> Fizz </li>';
+    } else if (i % 5 === 0) {
         console.log('Buzz');
-        result.innerHTML += '<li> Buzz </li>';
+        listItem += '<li> Buzz </li>';
     }
-    else{
+    else {
         console.log(i);
-        result.innerHTML += `<li> ${i} </li>`;
+        listItem += `<li> ${i} </li>`;
     }
 }
+
+result.innerHTML = listItem;
 
 
 
